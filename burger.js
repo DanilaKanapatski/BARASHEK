0973e4a119954776d6ea6814d33339d42b6b3b05
Danila Kanapatski <danila_konopatskiy@mail.ru>
2025-11-26 02:48:10 +0300

    fix4

--- burger.js
+++ burger.js
@@ -11,3 +11,14 @@ close.addEventListener("click", (e) => {
 
 console.log(burger);
 console.log(nav)
+
+const burgerMob = document.getElementById("header-mob-burger");
+const navMob = document.getElementById("header-nav-mob");
+const closeMob = document.getElementById("close-mob");
+
+burgerMob.addEventListener("click", (e) => {
+    navMob.classList.toggle("active");
+})
+closeMob.addEventListener("click", (e) => {
+    navMob.classList.toggle("active");
+})
